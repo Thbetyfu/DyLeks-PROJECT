88db5471025df95a217696c7d430fa565a8ff7e1
Thbetyfu <thoriq.a.taqy@gmail.com>
2026-06-11 01:49:33 +0800

    redesign: dyslexia-friendly UI/UX overhaul (evidence-based)

--- DyLeks/FE/pages/_app.tsx
+++ DyLeks/FE/pages/_app.tsx
@@ -1,18 +1,25 @@
 import type { AppProps } from 'next/app';
-import { Poppins } from 'next/font/google';
+import { Lexend } from 'next/font/google';
 import '../styles/globals.css';
 import { ThemeProvider } from '../contexts/ThemeContext';
 
-const poppins = Poppins({
+/*
+ * Lexend dipilih karena dirancang secara klinis oleh Thomas Jockin
+ * bersama Google.org untuk meningkatkan readability bagi pembaca
+ * dengan disleksia. Studi klinis menunjukkan peningkatan reading speed
+ * signifikan dibanding sans-serif generik.
+ */
+const lexend = Lexend({
   subsets: ['latin'],
-  weight: ['400', '600', '700'],
-  variable: '--font-poppins',
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-lexend',
+  display: 'swap',
 });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
-      <main className={poppins.className}>
+      <main className={lexend.className}>
         <Component {...pageProps} />
       </main>
     </ThemeProvider>
